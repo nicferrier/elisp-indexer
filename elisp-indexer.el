6ace56ce976ea125a9cb08196dfa7d373f86ccbb
Nic Ferrier <nferrier@ferrier.me.uk>
2014-10-14 17:12:27 +0100

    package header

--- elisp-indexer.el
+++ elisp-indexer.el
@@ -4,6 +4,8 @@
 
 ;; Author: Nic Ferrier <nferrier@ferrier.me.uk>
 ;; Keywords: lisp
+;; Version: 0.0.1
+;; Package-depends: ((dash "2.9.0"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
